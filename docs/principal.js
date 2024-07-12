69a000bd7fa3b5ab054f24a7b68658c307bd16a7
PipeMD <fmateluna@uc.cl>
2024-07-12 00:51:40 -0400

    entrega final

--- docs/principal.js
+++ docs/principal.js
@@ -1,36 +1,74 @@
 $(document).ready(function() {
-    const videos = $('video'); // Get all video elements
-
-    // Intersection Observer options
+    const videos = $('video'); 
+    
     const options = {
-        root: null, // Use the viewport as the root
+        root: null, 
         rootMargin: '0px',
-        threshold: 0.5 // Trigger when 50% of the video is visible
+        threshold: 0.5 
     };
 
-    // Intersection Observer callback function
+
     const callback = (entries, observer) => {
         entries.forEach(entry => {
-            const video = entry.target; // Get the video element
+            const video = entry.target; 
             if (entry.isIntersecting) {
                 video.play().catch(error => {
                     console.error('Autoplay failed:', error);
                 });
             } else {
-                video.pause(); // Pause the video when it leaves the view
-                video.currentTime = 0; // Reset the video to the beginning
+                video.pause(); 
+                video.currentTime = 0; 
             }
         });
     };
 
-    // Create Intersection Observer
+
     const observer = new IntersectionObserver(callback, options);
 
-    // Start observing each video element
+
     videos.each(function() {
         observer.observe(this);
     });
 });
+
+    $("#logo1").animate({ opacity: 1, top: "50px" }, 1000);
+
+    $("#logo2").animate({ opacity: 1, top: "50px" }, 1000);
+    
+
+
+   
+
+    $("#links_rrss a").hover(
+        function() {
+            $(this).animate({ color: "#f0781d" }, 300);
+        }, 
+        function() {
+            $(this).animate({ color: "#fff" }, 300);
+        }
+    );
+
+    $("#smooth a").hover(
+        function() {
+            $(this).animate({ color: "#f0781d" }, 300);
+        }, 
+        function() {
+            $(this).animate({ color: "#fff" }, 300);
+        }
+    );
+
+    $(window).scroll(function() {
+        $(".imagen, .flourish-embed").each(function() {
+            if ($(this).offset().top < $(window).scrollTop() + $(window).height()) {
+                $(this).animate({ opacity: 1, top: "0" }, 1000);
+            }
+        });
+    });
+
+
+
+
+
 
 
 
